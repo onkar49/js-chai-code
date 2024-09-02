d723e0ceb36f0325c9f571c330b83241a4540be9
onkar49 <63900543+onkar49@users.noreply.github.com>
2024-09-02 14:49:12 +0530

    first commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let a
+console.log(typeof a) //undefined
+
+let b = null
+console.log(typeof b) //object
+
+let c = 233
+console.log(typeof c) //number
+
+let d = 'dam'
+console.log(typeof d) //string
+ 
+let e = 3 + 5 + 4 + 1
+console.log(e) //13
+
+let f = 3 + 5 + '4' + 1
+console.log(f) //841
+
+let g = '3' + 5 + 4 + 1
+console.log(f) //3541
