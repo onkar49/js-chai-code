73bcfed376dab56f58b45705b026feb049cd7d9b
onkar49 <63900543+onkar49@users.noreply.github.com>
2024-09-02 15:37:11 +0530

    conversion file added

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,24 @@
+let score = '292'
+
+console.log(typeof score);      //string
+
+let conversion = Number(score)
+
+console.log(typeof conversion);   //number
+
+
+// "506" => 506
+// "123ad" => NaN
+// true => 1
+
+
+let loggedIn = ""
+
+console.log(typeof loggedIn);   
+
+let boolLoggedIn = Boolean(loggedIn)
+
+console.log(boolLoggedIn);
+
+// "" => false
+// "John" => true
